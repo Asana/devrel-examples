c3bb4315823125aff7e15eae8aa132c893c9b9fa
mattbramlage <mattbramlage@asana.com>
2018-05-17 15:26:06 -0700

    Check in hello world script

---
+++ python/hello_world/hello_world.py
@@ -0,0 +1,20 @@
+import os
+import asana
+
+# replace with your personal access token. 
+
+# Check and set our environment variables
+personal_access_token = None
+if 'ASANA_PAT' in os.environ:
+    personal_access_token = os.environ['ASANA_PAT']
+else:
+    print "No value for PAT in your console environment" 
+    print "Try running this as `ASANA_PAT=0/1234567890abcdef... python hello_world.py`
+    quit()
+
+client = asana.Client.access_token(personal_access_token)
+
+me = client.users.me()
+
+print "Hello world! " + "My name is " + me['name'] + " and I my primary Asana workspace is " + me['workspaces'][0]['name'] + "."
+
