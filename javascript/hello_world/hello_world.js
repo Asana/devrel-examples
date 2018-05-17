b765b24ce0c26ab2e69aa0cdb9d648ce1017540e
mattbramlage <mattbramlage@asana.com>
2018-05-17 16:20:42 -0700

    First version of JS Hello World

---
+++ javascript/hello_world/hello_world.js
@@ -0,0 +1,14 @@
+var asana = require('asana');
+
+// replace with your personal access token. 
+var personalAccessToken = '0/123456789....';
+
+// construct an Asana client
+var client = asana.Client.create().useAccessToken(personalAccessToken);
+
+// Get your user info
+client.users.me()
+  .then(function(me) {
+    // Print out your information
+    console.log('Hello world! ' + 'My name is ' + me.name + ' and my primary Asana workspace is ' + me.workspaces[0].name + '.');
+});
