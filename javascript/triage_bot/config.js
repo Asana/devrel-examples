42802908a92294230006db726f5a1c5ea1d58f58
jjschnei <jeffschneider@asana.com>
2019-07-31 15:05:59 -0700

    Add triage bot to devrel examples

---
+++ javascript/triage_bot/config.js
@@ -0,0 +1,16 @@
+let config = {
+  workspaceId: "15793206719",
+  designRequestProjectId: "1132780943356035",
+  //ids of designers who are fulfilling design requests
+  designers: ["658395820194976", "180015866142454"]
+}
+
+module.exports = config;
+
+
+// TODO:
+// Use an Asana task as the config file i.e. put all of the config 
+// data in an Asana task and then use the API to fetch the task. 
+// This will allow Asana users to update the configuraiton of the 
+// triage bot without touching this code. 
+
