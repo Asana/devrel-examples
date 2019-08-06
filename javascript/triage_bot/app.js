ce113eeb48ab960f8c1b5462483188c99950b8a4
jjschnei <jeffschneider@asana.com>
2019-08-06 14:54:05 -0700

    clarify and punctuate comments

--- javascript/triage_bot/app.js
+++ javascript/triage_bot/app.js
@@ -8,14 +8,15 @@ const accessToken = process.env.triage_bot_pat;
 // two deprecations (moving to string ids and changing how sections function).
 // You can learn about our deprecations framework in our docs
 // (https://asana.com/developers/documentation/getting-started/deprecations).
-// To prevent my app from breaking when the deprecations are finalized, I'm
+// To prevent this bot from breaking when the deprecations are finalized, I'm
 // passing headers to enable the new API behavior for string gids and sections.
 const deprecationHeaders = {"defaultHeaders": {"asana-enable": "new_sections,string_ids"}};
 
 // Create Asana client using PAT and deprecation headers.
 const client = asana.Client.create(deprecationHeaders).useAccessToken(accessToken);
 
-// Request delay in ms
+// Request delay in ms. We will use this to avoid hitting Asana rate limits:
+// https://asana.com/developers/documentation/getting-started/rate-limits
 const delay = 200;
 
 
@@ -35,8 +36,8 @@ function getUnassignedTasks() {
   });
 }
 
-// Helper function to randomly shuffle an array.
-// Used to randomize the Asana users fulfilling requests.
+// Helper function to randomly shuffle an array. We will use it to 
+// randomize the list of Asana users fulfilling requests.
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -47,40 +48,38 @@ function shuffleArray(array) {
   return array;
 }
 
-// Helper function to assign a task in Asana
+// Helper function to assign a task in Asana.
 function assignTask(taskStringId, assigneeStringId) {
   client.tasks.update(taskStringId, {assignee: assigneeStringId});
 }
 
-// Takes array of unassigned tasks and array of designers and assigns a designer
-// to each task. This uses the const delay to determine how fast to send requests.
+// Takes an array of unassigned tasks and an array of designers 
+// and assigns a designer to each task. 
 function randomAssigner(unassignedTasks) {
   let shuffledDesigners = shuffleArray(config.designers);
   let numDesigners = shuffledDesigners.length;
-
-  // Let's use an interval to loop, so we control how quickly requests are sent
+  // We will use an interval to control how quickly requests are sent 
+  // in order to avoid being rate limited. The interval uses the 
+  // const delay, which determines how long to wait between requests.
   let index = 0;
   let interval = setInterval(function() {
     assignTask(unassignedTasks[index].gid, shuffledDesigners[index % numDesigners]);
     index++;
-
-    // When our index reaches the end, we're done
     if (index >= unassignedTasks.length) {
       clearInterval(interval);
-
       console.log("You've assigned " + unassignedTasks.length + " new design requests");
     }
   }, delay);
 }
 
-// Run the triage bot:
+// Run the triage bot.
 getUnassignedTasks();
 
 
-// TODO:
-// Ping overdue tasks
-// Use searchInWorkplace to find tasks that are overdue in the requests project
-// Ping the over due tasks with a snarky message.
+// Potential enhancements to add to the bot:
+//   -Find tasks that are nearly due and remind the assignee.
+//   -Ping over due tasks with a snarky message.
+//   -Use web hooks to keep the triage bot synced in real time. 
 
 
 
