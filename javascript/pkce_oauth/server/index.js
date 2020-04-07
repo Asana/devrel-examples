9f272639d08d26e0bed8115adb0943fcb8da1118
Ross Grambo <rossgrambo@asana.com>
2020-04-07 13:14:18 -0700

    Updated xml request to fetch

--- javascript/pkce_oauth/server/index.js
+++ javascript/pkce_oauth/server/index.js
@@ -7,7 +7,6 @@ const functions = require('firebase-functions');
 
 const challengeMethod = "S256";
 const authorizationGrantType = "authorization_code";
-const refreshGrantType = "refresh_token";
 
 const clientId = process.env.client_id;
 const clientSecret = process.env.client_secret;
@@ -16,7 +15,6 @@ const frontEndLocation = process.env.front_end_location;
 const tokenExchangeEndpoint = process.env.token_exchange_endpoint;
 
 const stateCache = {};
-const tokenCache = {};
 
 const app = express();
 
@@ -49,9 +47,6 @@ app.get('/first-step-auth', (req, res) => {
     // Save that the state and codeVerifier were given to the same person
     stateCache[state] = codeVerifier;
 
-    console.log(codeVerifier);
-    console.log(codeChallenge);
-
     // Create the result to send to the client
     const result = {
         codeChallenge: encodeURI(codeChallenge),
@@ -63,7 +58,7 @@ app.get('/first-step-auth', (req, res) => {
 
     // Store state as a cookie on the client
     res.cookie('state', state, {
-        maxAge: 15 * 60000,             // cookie will be removed after 15 min
+        maxAge: 15 * 60 * 1000,         // cookie will be removed after 15 min
         httpOnly: true,                 // cookie cannot be read by browser javascript
         secure: true,                   // cookie can only be used with HTTPS
         path: process.env.redirect_uri  // cookie can only be used with our uri
@@ -75,16 +70,18 @@ app.get('/first-step-auth', (req, res) => {
 // Second step.
 // We need to get the state and code from the client.
 // We will verify the state is valid, and send the code to the auth server.
-// We then store the refresh token locally and return the access token to the client.
 app.get('/second-step-auth', (req, res) => {
     console.log('In second-step-auth');
 
     const state = req.cookies.state;
 
     // This check is used to prevent XSS attacks. If you have implemented PKCE correctly
-    // you do not need to do this check, as Asana will protect the user during the token
+    // you should not need to do this check, as Asana will protect the user during the token
     // exchange with the codeVerifier.
     if (req.query.state !== state) {
+        console.log('Failed state verification');
+        console.log(req.query.state + " VS " + state);
+        res.statusMessage = "State params did not match";
         res.status(401);
         res.send();
         return;
@@ -105,7 +102,8 @@ app.get('/second-step-auth', (req, res) => {
         code: code,
     };
 
-    // Lets ask the auth server for an access and refresh token
+    console.log('Requesting token from Asana');
+    // Lets ask the auth server for an access token
     request.post(tokenExchangeEndpoint, {
         form: requestBody
     }, (error, responseObj, responseBody) => {
@@ -113,72 +111,32 @@ app.get('/second-step-auth', (req, res) => {
 
         if (error) {
             console.error(error);
+            res.statusMessage = "Asana did not approve the request.";
             res.status(401);
             res.send();
             return;
         }
 
-        console.log(codeVerifier);
-        console.log(responseObj);
-
         handleNewToken(JSON.parse(responseBody), res);
     });
 });
 
-// This allows the front end app to request a new token if their current token is not expired.
-// This is an oversimplified version of how refresh-token handling should work.
-app.get('/refresh-token', (req, res) => {
-    console.log('In refresh-token');
-
-    const access_token = req.cookies.access_token;
-    const refreshTokenObj = tokenCache[access_token];
-
-    if (refreshTokenObj !== null && Date.now() < (refreshTokenObj.createdAt + refreshTokenObj.expiresIn)) {
-        const requestBody = {
-            grant_type: refreshGrantType,
-            client_id: clientId,
-            client_secret: clientSecret,
-            refresh_token: refreshTokenObj.refreshToken,
-        };
-
-        // Ask the auth server for a new token
-        request.post(tokenExchangeEndpoint, {
-            form: requestBody
-        }, (error, responseObj, responseBody) => {
-            if (error) {
-                console.error(error);
-                res.status(401);
-                res.send();
-                return;
-            }
-
-            handleNewToken(JSON.parse(responseBody), res);
-        });
-    } else {
-        res.statusCode = '401';
-        res.send();
-    }
-});
-
 // Handles a response from the auth server.
 function handleNewToken(body, res) {
-    console.log(body);
-
     const accessToken = body.access_token;
     const expiresIn = body.expires_in;
-    const refreshToken = body.refresh_token;
     const userData = body.data;
 
-    console.log("Storing access & refresh token for " + userData.name + " (" + userData.gid + ")");
+    // const refreshToken = body.refresh_token;
+    // If you want to persist logins, you should use SQL, NoSQL or some other persistent database to store refresh
+    // tokens.
 
-    res.cookie("access_token", accessToken);
+    console.log("Storing access token for " + userData.name + " (" + userData.gid + ")");
 
-
-    tokenCache[accessToken] = {
-        createdAt: Date.now(),
-        expiresIn: expiresIn,
-        refreshToken: refreshToken
-    };
+    res.cookie("access_token", accessToken, {
+        maxAge: expiresIn * 1000,       // express uses milliseconds while Asana gives seconds
+        path: process.env.redirect_uri  // cookie should only be used with our uri
+    });
 
     res.redirect(redirectUri);
 }
