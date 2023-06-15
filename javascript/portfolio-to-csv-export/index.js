cdf3e53638b74c397586be205dd3303cf75a8bb9
Andrew Wong <andrewwong@asana.com>
2023-06-14 17:34:27 -0700

    Refactor Portfolio to CSV Export app.

--- javascript/portfolio-to-csv-export/index.js
+++ javascript/portfolio-to-csv-export/index.js
@@ -1,19 +1,20 @@
 const runExport = async () => {
   // Start a timer (this is purely for performance logging)
-  let start = new Date();
+  const start = new Date();
 
   // Reset elements on the page
-  document.getElementById("submit").disabled = true;
-  document.getElementById("submit").value = "Please wait...";
+  const submitBtn = document.getElementById("submit");
+  submitBtn.disabled = true;
+  submitBtn.value = "Please wait...";
   document.getElementById("errorbox").innerText = "";
 
   // Parse user input from the page
-  let pat = document.getElementById("pat").value;
-  let portfolio_gid = document.getElementById("portfolio").value;
+  const pat = document.getElementById("pat").value;
+  const portfolio_gid = document.getElementById("portfolio").value;
 
   // Validate fields (i.e., to not be empty)
-  if (pat == "" || portfolio_gid == "") {
-    let message = "Please provide input to both fields to continue";
+  if (pat === "" || portfolio_gid === "") {
+    const message = "Please provide input to both fields to continue";
     // Display a red error text box on the page (see utils.js)
     postError(message);
     return;
@@ -22,7 +23,7 @@ const runExport = async () => {
   // Perform basic validation of the provided portfolio GID
   // For more information on portfolios, see: https://developers.asana.com/reference/portfolios
   if (!portfolio_gid || isNaN(portfolio_gid) || portfolio_gid.length < 3) {
-    let message =
+    const message =
       "Your portfolio GID is invalid. Please check it and try again";
     postError(message);
     return;
@@ -32,13 +33,13 @@ const runExport = async () => {
   // For more information on personal access tokens, see: https://developers.asana.com/docs/personal-access-token
   // For more information on this API endpoint, see: https://developers.asana.com/reference/getuser
   const httpHeaders = { Authorization: `Bearer ${pat}` };
-  let resp = await fetch(`https://app.asana.com/api/1.0/users/me`, {
+  const resp = await fetch(`https://app.asana.com/api/1.0/users/me`, {
     headers: httpHeaders,
   });
 
   // Display an error if we do not receive a 200 OK response
   if (!resp.ok) {
-    let message =
+    const message =
       "Your personal access token is invalid. For documentation, see: https://developers.asana.com/docs/personal-access-token";
     postError(message);
     return;
@@ -47,39 +48,37 @@ const runExport = async () => {
   // Run the asynchronous portfolioExtract() function (see utils.js)
   // This returns an array of objects, each representing a project
   // Note that the resulting array may include duplicate projects (e.g., projects in multiple portfolios)
-  let projects = await extractProjectsFromPortfolio(
+  const projects = await extractProjectsFromPortfolio(
     portfolio_gid,
-    {},
     httpHeaders
   );
 
-  // Create a consolidated list of all headers
-  // To prevent adding duplicated projects, we use a Set because since any value in the Set can only occur once
-  let headerSet = new Set();
+  // Create a consolidated list of all headers (i.e., titles for each column)
+  // To prevent adding duplicated projects, we use a Set because any value in the Set can only occur once
+  const headerSet = new Set();
 
   // Create a consolidated list of all projects
   // finalProjects itself is an object in which project IDs (keys) are mapped to project objects (values)
-  let finalProjects = {};
+  const finalProjects = {};
 
   // Iterate through all projects
-  for (let i = 0; i < projects.length; i++) {
+  for (const project of projects) {
+    const projectId = project["Project ID"];
     // Check if we've already mapped the project
     // (i.e., if the project ID exists as a key in finalProjects)
-    if (projects[i]["Project ID"] in finalProjects) {
+    if (projectId in finalProjects) {
       // If so, just add any additional properties...
-      finalProjects[projects[i]["Project ID"]] = {
-        ...projects[i],
-        ...finalProjects[projects[i]["Project ID"]],
+      finalProjects[projectId] = {
+        ...project,
+        ...finalProjects[projectId],
       };
     } else {
       // ...otherwise, add the new project into finalProjects
-      finalProjects[projects[i]["Project ID"]] = {
-        ...projects[i],
-      };
+      finalProjects[projectId] = { ...project };
     }
 
-    for (const property in projects[i]) {
-      // Add all properties (including custom fields) to our set of headers
+    for (const property in project) {
+      // Add all properties to our set of headers
       // Such properties include Project ID, Project name, etc.
       headerSet.add(property);
     }
@@ -87,23 +86,23 @@ const runExport = async () => {
 
   // Get the consolidated list of projects
   // This is an array of objects, each representing a project
-  let finalProjectList = Object.values(finalProjects);
+  const finalProjectList = Object.values(finalProjects);
 
-  // Create the list of headers (i.e., titles for each column)
+  // Create the list of headers
   // This is an array of strings, each representing a header
-  let csvHeaders = [...headerSet];
+  const csvHeaders = [...headerSet];
 
   // Export the data to CSV and download it (see utils.js)
-  // Arguments include our list of CSV headers, the consolidated list of projects, and "PortfolioExport" as the file name
+  // Arguments include our list of CSV headers and the consolidated list of projects
   exportToCsv(csvHeaders, finalProjectList);
 
   // Log and display the finish time to the user
-  let duration = new Date() - start;
+  const duration = new Date() - start;
   alert(`Finished in ${duration} ms`);
 
   // Reset submit button to allow for another submission
-  document.getElementById("submit").disabled = false;
-  document.getElementById("submit").value = "Generate CSV";
+  submitBtn.disabled = false;
+  submitBtn.value = "Generate CSV";
 
   return false;
 };
--- javascript/portfolio-to-csv-export/utils.js
+++ javascript/portfolio-to-csv-export/utils.js
@@ -1,55 +1,48 @@
-/* ########################### Fetching and parsing data ########################### */
+// ================================================
+// Fetching and parsing data
+// ================================================
 
 // extractProjectsFromPortfolio() returns a list of all projects under that portfolio (including projects in nested portfolios)
 // This is a recursive function that takes in:
 // - A portfolio GID
 // - Default HTTP headers (including authorization)
 async function extractProjectsFromPortfolio(portfolio_gid, httpHeaders) {
-  let items = [];
-
-  // Get all items from the portfolio
   try {
-    items = await getAsanaPortfolioItems(portfolio_gid, {
+    // Get all items from the portfolio
+    let items = await getAsanaPortfolioItems(portfolio_gid, {
       headers: httpHeaders,
     });
+    let projects = [];
+    let portfolioPromises = [];
+
+    // Iterate through items
+    for (let item of items) {
+      // If the item is a project, flatten/map the standard project fields, then add it to our list of projects
+      if (item["resource_type"] === "project") {
+        let newItem = { ...flattenProjectFields(item) };
+        projects.push(newItem);
+      } else if (item["resource_type"] === "portfolio") {
+        // If the item is a portfolio, run extractProjectsFromPortfolio() recursively
+        portfolioPromises.push(
+          extractProjectsFromPortfolio(item["gid"], httpHeaders)
+        );
+      }
+    }
+
+    // Wait for all the nested portfolio responses to come back...
+    let portfolioResults = await Promise.all(portfolioPromises);
+    // ...then add those projects to the list
+    projects.push(...portfolioResults.flat(3));
+
+    // Since the above operation can create nested arrays, we flatten them out
+    // The returned value is an array of objects, each representing a project
+    return projects.flat(3);
   } catch (error) {
     console.log(error);
     postError(
       "Something went wrong... inpect the page to view the dev console or wait and try again"
     );
   }
-
-  let projects = [];
-  let portfolioPromises = [];
-
-  // Iterate through items
-  for (let i = 0; i < items.length; i++) {
-    let item = items[i];
-
-    if (item["resource_type"] == "project") {
-      // If the item is a project, flatten/map the standard project fields, then add it to our list of projects
-      let newItem = {
-        ...flattenProjectFields(item),
-      };
-      projects.push(newItem);
-    } else if (item["resource_type"] == "portfolio") {
-      // If the item is a portfolio, run this function recursively
-      portfolioPromises.push(
-        extractProjectsFromPortfolio(item["gid"], httpHeaders)
-      );
-    }
-  }
-
-  // Wait for all the nested portfolio responses to come back...
-  let portfolioResults = await Promise.all(portfolioPromises);
-  // ...then add those projects to the list
-  projects.push(portfolioResults);
-
-  // Since the above can create nested arrays, we flatten them out
-  let flatProjects = projects.flat(3);
-
-  // flatProjects is an array of objects, each representing a project
-  return flatProjects;
 }
 
 // getAsanaPortfolioItems() makes a request against the Asana API to get all the projects and portfolios from a portfolio
@@ -57,13 +50,13 @@ async function extractProjectsFromPortfolio(portfolio_gid, httpHeaders) {
 async function getAsanaPortfolioItems(portfolio_gid, headers) {
   // Max retries for rate limited calls
   // For more information on rate limits, see: https://developers.asana.com/docs/rate-limits
+  const maxRetries = 10;
   let retryCounter = 0;
-  let maxRetries = 10;
 
   // While we haven't finished the request, keep trying
   while (retryCounter < maxRetries) {
     // Get items from the portfolio with the exact fields we want
-    // For more information on this API endpoint, see: https://developers.asana.com/reference/getitemsforportfolio
+    // For more information on this API endpoint, see:
     const resp = await fetch(
       `https://app.asana.com/api/1.0/portfolios/${portfolio_gid}/items?opt_fields=name,resource_type,archived,color,created_at,current_status_update.(created_by.name|status_type|created_at|text),notes,modified_at,public,owner.name,start_on,due_on`,
       headers
@@ -71,80 +64,71 @@ async function getAsanaPortfolioItems(portfolio_gid, headers) {
 
     // The request succeeds succeeded, return the response from the API (i.e., portfolio items)
     if (resp.ok) {
-      // results is an array of objects (where each object is either a project or portfolio)
       const results = await resp.json();
-      return results["data"];
+      return results.data;
     }
 
     // If there is an error (due to lack of permissions, etc.), stop the iteration and display an error
     if (resp.status >= 400 && resp.status != 429 && resp.status != 500) {
-      document.getElementById("errorbox").innerHTML +=
-        errorCodeMap[resp.status] || "";
+      const errorMessage = errorCodeMap[resp.status] || "";
+      document.getElementById("errorbox").innerHTML += errorMessage;
       break;
     }
 
     // Back off exponentially in case we're hitting rate limits (i.e., wait before retrying)
     retryCounter++;
-    let wait_time = retryCounter * retryCounter * 120;
-
-    await new Promise((r) => setTimeout(r, wait_time));
+    const wait_time = retryCounter * retryCounter * 120;
+    await new Promise((resolve) => setTimeout(resolve, wait_time));
   }
 
   // Return an empty array by default
   return [];
 }
 
-/* ########################### CSV generation ########################### */
+// ================================================
+// CSV generation
+// ================================================
 
 // exportToCsv() exports project content to a CSV file
 function exportToCsv(headers, projects) {
   let csvContent = "";
 
   // Write the header row
-  csvContent += headers.map((h) => '"' + h + '"').join(",");
-  csvContent += "\n";
+  csvContent += headers.map((h) => `"${h}"`).join(",") + "\n";
 
   // Map each project as a new row
   let projectsCsvData = projects
-    .map((project) => {
-      let rowString = "";
-      rowString += headers.map((key) => project[key] || "").join(",");
-
-      return rowString;
-    })
+    .map((project) => headers.map((key) => project[key] || "").join(","))
     .join("\n");
 
   // Join the content to the headers
   csvContent += projectsCsvData;
 
   // Create the file with the data
-  let blob = new Blob([csvContent], { type: "text/csv" });
-  let href = window.URL.createObjectURL(blob);
+  const blob = new Blob([csvContent], { type: "text/csv" });
+  const href = window.URL.createObjectURL(blob);
 
   // Force download
-  let link = document.createElement("a");
+  const link = document.createElement("a");
   link.setAttribute("href", href);
   link.setAttribute("download", "portfolio_export.csv");
   document.body.appendChild(link);
   link.click();
   window.URL.revokeObjectURL(href);
-
-  return;
 }
 
-/* ########################### Data formatting ########################### */
+// ================================================
+// Data formatting
+// ================================================
 
 // flattenProjectFields() maps and formats API fields to descriptive reporting headers
 function flattenProjectFields(project) {
-  newProject = {
+  const newProject = {
     "Project ID": escapeText(project["gid"] || ""),
     Name: escapeText(project["name"] || ""),
     Notes: escapeText(project["notes"] || ""),
     Color: escapeText(project["color"] || ""),
-    "Status color":
-      project["current_status_update"]?.["status_type"] in statusTextMap
-        ? statusTextMap[project["current_status_update"]?.["status_type"]]
-        : "",
+    "Status color": getStatusColor(project),
     "Status update": escapeText(
       project["current_status_update"]?.["text"] || ""
     ),
@@ -166,11 +150,19 @@ function flattenProjectFields(project) {
 
 // escapeText() wraps text in double quote (") text delimiters and escapes any double quotes
 function escapeText(text) {
-  let newText = text.replace(/"/g, '""');
-  return '"' + newText + '"';
+  const newText = text.replace(/"/g, '""');
+  return `"${newText}"`;
 }
 
-/* ########################### Error handling ########################### */
+// getStatusColor() handles the mapping of status types to colors (i.e., determines the "Status color" field)
+function getStatusColor(project) {
+  const statusType = project["current_status_update"]?.["status_type"];
+  return statusType in statusTextMap ? statusTextMap[statusType] : "";
+}
+
+// ================================================
+// Error handling
+// ================================================
 
 // postError() displays a red error text box on the page
 function postError(message) {
@@ -182,7 +174,9 @@ function postError(message) {
   return;
 }
 
-/* ########################### Maps ########################### */
+// ================================================
+// Maps
+// ================================================
 
 // Maps API error codes to useful information
 const errorCodeMap = {
