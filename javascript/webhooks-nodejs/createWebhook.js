a44ea37d0be89e65637bdfebaa73c00ce33b078d
Andrew Wong <andrewwong@asana.com>
2024-10-14 13:07:55 -0700

    Fix placeholder formatting.

--- javascript/webhooks-nodejs/createWebhook.js
+++ javascript/webhooks-nodejs/createWebhook.js
@@ -99,7 +99,7 @@ async function createWebhook(targetUri, objectId, filter, resourceType) {
 
 // TODO: Replace these values with your target URI, object ID, filter, and resource type
 const targetUri = "<YOUR_URL_HERE>/receiveWebhook"; // The webhook server's public endpoint for receiving webhooks (view README for an example)
-const objectId = "OBJECT_ID_HERE"; // The Asana object ID you want to track (e.g., a task gid)
+const objectId = "<OBJECT_ID_HERE>"; // The Asana object ID you want to track (e.g., a task gid)
 const filter = "changed"; // The action to filter for (documentation: https://developers.asana.com/docs/webhooks-guide#actions)
 const resourceType = "task"; // Specify the resource type (e.g., task, project)
 
